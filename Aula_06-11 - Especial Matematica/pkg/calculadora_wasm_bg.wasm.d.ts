8ba40248de18d0b776f4a2ef4d7bf484cd2fd9c0
Kaua-Melchior <kauapro007brasileiro@gmail.com>
2026-06-17 17:53:58 -0300

    feat: adiciona Aula 06-11 e habilita tracking de WebAssembly no hub

---
+++ Aula_06-11 - Especial Matematica/pkg/calculadora_wasm_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const calcular_combinacao: (a: number, b: number, c: number, d: number) => [number, number];
+export const calcular_fatorial: (a: number, b: number) => [number, number];
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
